cf53d0546f3def98463f0a1e1547ca4879a7451a
FVL <langvincent1@web.de>
2024-05-16 22:03:12 +0200

    setting the parameters for the logistic regression meta grid search

--- logistic_regression.py
+++ logistic_regression.py
@@ -42,9 +42,9 @@ print(sorted(grid_clf.cv_results_.keys()))
 
 # %%
 
-vectorizer_dict = {"tfidf": {'max_df':0.8}}#, "count": {'max_features': 5000, 'max_df':0.8}}
-parameters = {'multi_class':('ovr', 'multinomial'), 'penalty': ('l2',), 'solver': ('lbfgs',), 'max_iter': (1000,)}
-# parameters = {'multi_class':('ovr', 'multinomial'), 'penalty': ('l1', 'l2', 'elasticnet'), 'solver': ('newton-cg', 'sag', 'saga', 'lbfgs'), 'max_iter': (1000,)}
+vectorizer_dict = {"word2vec": {}, "fasttext": {}, "spacy": {}, "tfidf": {'max_df':0.8}, "count": {'max_df':0.8}, }
+#parameters = {'multi_class':('ovr', 'multinomial'), 'penalty': ('l2',), 'solver': ('lbfgs',), 'max_iter': (1000,)}
+parameters = {'multi_class':('ovr', 'multinomial'), 'penalty': ('l1', 'l2', 'elasticnet'), 'solver': ('newton-cg', 'sag', 'saga', 'lbfgs'), 'max_iter': (1000,)}
 
 grid_search_result = meta_grid_search(model=model, vectorizer_dict=vectorizer_dict, parameters=parameters, 
                             train_data=train_data, val_data=val_data, test_data=test_data)
