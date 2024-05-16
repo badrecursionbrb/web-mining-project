a17196f243c93d4aad6a0df1c02fd64b20b17cda
FVL <langvincent1@web.de>
2024-05-16 17:01:37 +0200

    updated analyze of model

--- naive_bayes_new.py
+++ naive_bayes_new.py
@@ -5,7 +5,7 @@ import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn.naive_bayes import MultinomialNB, CategoricalNB
 from sklearn.metrics import accuracy_score, f1_score
-from functions import load_data, load_datasets, VectorizerWrapper
+from functions import load_data, load_datasets, VectorizerWrapper, analyze_model
 from sklearn.model_selection import GridSearchCV
 
 
@@ -21,29 +21,18 @@ X_train = vectorizer.fit_transform(train_data['tweet'])
 # Transform the validation and test data
 X_val = vectorizer.transform(val_data['tweet'])
 X_test = vectorizer.transform(test_data['tweet'])
-
+train_labels = train_data['label']
+val_labels = val_data['label']
+test_labels = test_data['label']
 
 #%%
 # Create and train the Naive Bayes model
 model = MultinomialNB()
 model.fit(X_train, train_data['label'])
 
-# Predict on validation data
-val_predictions = model.predict(X_val)
-val_f1 = f1_score(val_data['label'], val_predictions, average="weighted")
-print(f'Validation F1: {val_f1:.2f}')
-
-val_accuracy = accuracy_score(val_data['label'], val_predictions)
-print(f'Validation Accuracy: {val_accuracy:.2f}')
-
-# %%
-# Predict on test data
-test_predictions = model.predict(X_test)
-test_f1 = f1_score(test_data['label'], test_predictions, average="weighted")
-print(f'Test F1: {test_f1:.2f}')
-
-test_accuracy = accuracy_score(test_data['label'], test_predictions)
-print(f'Test Accuracy: {test_accuracy:.2f}')
+#%%
+# Analyze the model 
+analyze_model(model=model, X_val=X_val, val_labels=val_labels, X_test=X_test, test_labels=test_labels)
 
 #%% 
 # grid search for Multinomial NB 
